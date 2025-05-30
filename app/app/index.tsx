d56fc645487665a42b696bf6a892b2b6b2126c9a
mkki <devmkki@gmail.com>
2025-05-30 17:24:32 +0900

    feat: expo 메시지 송수신 로직 추가

--- app/app/index.tsx
+++ app/app/index.tsx
@@ -1,11 +1,61 @@
+import { useRef } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { WebView } from 'react-native-webview';
 
+import * as Location from 'expo-location';
+
 export default function Index() {
+  const webviewRef = useRef<WebView>(null);
+
+  const sendLocationToWebView = async () => {
+    const { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      alert('위치권한을 허용해주세요.');
+      return;
+    }
+
+    const location = await Location.getCurrentPositionAsync({});
+    const message = JSON.stringify({
+      type: 'CURRENT_LOCATION',
+      payload: {
+        lat: location.coords.latitude,
+        lng: location.coords.longitude,
+      },
+    });
+
+    console.log('Sending location to web:', message);
+    webviewRef.current?.postMessage(message);
+  };
+
+  const handleWebMessage = (event: any) => {
+    try {
+      const message = JSON.parse(event.nativeEvent.data);
+      console.log('Message received from web:', message);
+
+      switch (message.type) {
+        case 'NAVER_MAP_LOADED':
+          console.log('Naver Map is ready');
+          sendLocationToWebView();
+          break;
+          case 'GET_CURRENT_LOCATION':
+          sendLocationToWebView();
+          break;
+        default:
+          break;
+      }
+    } catch {
+      console.log('Message from web (non-JSON):', event.nativeEvent.data);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <WebView
+        ref={webviewRef}
         source={{ uri: 'http://169.254.157.72:5173/' }}
+        originWhitelist={['*']}
+        javaScriptEnabled={true}
+        onMessage={handleWebMessage}
       />
     </View>
   );
