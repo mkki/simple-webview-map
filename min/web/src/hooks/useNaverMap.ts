76e18f4bbada2bccd0d1e2d92543985655837c75
mkki <devmkki@gmail.com>
2025-06-06 01:37:28 +0900

    refactor: 네이버 맵 초기화 코드 성능 최적화

--- min/web/src/hooks/useNaverMap.ts
+++ min/web/src/hooks/useNaverMap.ts
@@ -1,5 +1,5 @@
 import { v4 as uuidv4 } from 'uuid';
-import { useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import {
   loadNaverMapsScript,
   searchCoordinateToAddress,
@@ -28,6 +28,53 @@ export const useNaverMap = ({
   const [isMapInitialized, setIsMapInitialized] = useState(false);
   const isWebView = useIsWebView();
 
+  const handleMapClick = useCallback(async (e: naver.maps.PointerEvent) => {
+    if (!mapRef.current) return;
+
+    const lat = e.coord.y;
+    const lng = e.coord.x;
+
+    if (!markerRef.current) {
+      const marker = new naver.maps.Marker({
+        position: e.coord,
+        map: mapRef.current,
+        icon: {
+          url: '/icons/Marker.svg',
+          size: new naver.maps.Size(46, 46),
+          anchor: new naver.maps.Point(23, 46),
+          scaledSize: new naver.maps.Size(46, 46),
+        },
+      });
+
+      markerRef.current = marker;
+    } else {
+      markerRef.current.setPosition(e.coord);
+    }
+
+    let address;
+
+    try {
+      const response = await searchCoordinateToAddress(lat, lng);
+      const rawAddress = response.v2.address.roadAddress
+        ? response.v2.address.roadAddress
+        : response.v2.address.jibunAddress;
+      address = rawAddress?.replace(/\s{2,}/g, ' ');
+    } catch (error) {
+      console.error('Failed to fetch address information:', error);
+    }
+
+    const markerInfo: MarkerInfo = {
+      id: uuidv4(),
+      coord: {
+        lat,
+        lng,
+      },
+      address,
+    };
+
+    onAddMarker(markerInfo);
+  }, [mapRef, onAddMarker]);
+
   useEffect(() => {
     const initializeMap = async () => {
       if (!containerRef.current || mapRef.current) return;
@@ -45,53 +92,6 @@ export const useNaverMap = ({
           ...restOptions,
           center: center && new naver.maps.LatLng(center.lat, center.lng),
         });
-
-        const handleMapClick = async (e: naver.maps.PointerEvent) => {
-          if (!mapRef.current) return;
-
-          const lat = e.coord.y;
-          const lng = e.coord.x;
-
-          if (!markerRef.current) {
-            const marker = new naver.maps.Marker({
-              position: e.coord,
-              map: mapRef.current,
-              icon: {
-                url: '/icons/Marker.svg',
-                size: new naver.maps.Size(46, 46),
-                anchor: new naver.maps.Point(23, 46),
-                scaledSize: new naver.maps.Size(46, 46),
-              },
-            });
-
-            markerRef.current = marker;
-          } else {
-            markerRef.current.setPosition(e.coord);
-          }
-
-          let address;
-
-          try {
-            const response = await searchCoordinateToAddress(lat, lng);
-            const rawAddress = response.v2.address.roadAddress
-              ? response.v2.address.roadAddress
-              : response.v2.address.jibunAddress;
-            address = rawAddress?.replace(/\s{2,}/g, ' ');
-          } catch (error) {
-            console.error('Failed to fetch address information:', error);
-          }
-
-          const markerInfo: MarkerInfo = {
-            id: uuidv4(),
-            coord: {
-              lat,
-              lng,
-            },
-            address,
-          };
-
-          onAddMarker(markerInfo);
-        };
 
         naver.maps.Event.addListener(mapRef.current, 'click', handleMapClick);
 
@@ -111,7 +111,13 @@ export const useNaverMap = ({
     };
 
     initializeMap();
-  }, [mapRef, options, isWebView, onAddMarker]);
+
+    return () => {
+      if (mapRef.current) {
+        naver.maps.Event.clearInstanceListeners(mapRef.current);
+      }
+    };
+  }, [mapRef, options, isWebView, handleMapClick]);
 
   useNaverMapFavoriteMarkers({
     mapRef,
